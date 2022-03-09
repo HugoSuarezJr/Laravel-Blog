46ccff7f8fe1e2e6a1869302d554602e10470818
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-03-09 13:58:30 -0500

    Create routes and views for blogs

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,19 +7,19 @@
 <body>
 
     <article>
-        <h1><a href='/post'>My First Post</a></h1>
+        <h1><a href='/posts/my-first-post'>My First Post</a></h1>
 
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam corrupti explicabo magnam asperiores atque ab minus odit expedita facere similique voluptatem eligendi laborum amet ipsum, veniam quod cum vero nisi.</p>
     </article>
 
     <article>
-        <h1><a href='/post'>My Second Post</a></h1>
+        <h1><a href='/posts/my-second-post'>My Second Post</a></h1>
 
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam corrupti explicabo magnam asperiores atque ab minus odit expedita facere similique voluptatem eligendi laborum amet ipsum, veniam quod cum vero nisi.</p>
     </article>
 
     <article>
-        <h1><a href='/post'>My Third Post</a></h1>
+        <h1><a href='/posts/my-third-post'>My Third Post</a></h1>
 
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam corrupti explicabo magnam asperiores atque ab minus odit expedita facere similique voluptatem eligendi laborum amet ipsum, veniam quod cum vero nisi.</p>
     </article>
