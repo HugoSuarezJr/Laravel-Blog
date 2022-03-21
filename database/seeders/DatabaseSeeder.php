c3fa9c00926bbedcde5b1154b7493ab2406643f4
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-03-21 15:07:23 -0400

    Using factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,43 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        $user = User::factory()->create([
+            'name' => 'John Doe'
         ]);
-
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
-
-        $category = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $category->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet.',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, voluptatem nesciunt? Nam eligendi dicta nemo exercitationem, repellat molestiae consequuntur magni. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui voluptate eveniet nulla, enim odio perspiciatis omnis placeat temporibus fugit rem.</p>'
-        ]);
-    
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $category->id,
-            'title' => 'My Personal Post',
-            'slug' => 'my-personal-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet.',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, voluptatem nesciunt? Nam eligendi dicta nemo exercitationem, repellat molestiae consequuntur magni. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui voluptate eveniet nulla, enim odio perspiciatis omnis placeat temporibus fugit rem.</p>'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
     }
 }
