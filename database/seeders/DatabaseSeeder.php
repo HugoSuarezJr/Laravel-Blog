95f3625a077fd69f3dfc9ae4013ab3b273ff5447
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-03-21 14:11:16 -0400

    Connect each post with a User in a database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 use Illuminate\Database\Seeder;
 
@@ -13,6 +16,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Post::truncate();
+        Category::truncate();
+
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        $category = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $category->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-family-post',
+            'excerpt' => 'Lorem ipsum dolor sit amet.',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, voluptatem nesciunt? Nam eligendi dicta nemo exercitationem, repellat molestiae consequuntur magni. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui voluptate eveniet nulla, enim odio perspiciatis omnis placeat temporibus fugit rem.</p>'
+        ]);
+    
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $category->id,
+            'title' => 'My Personal Post',
+            'slug' => 'my-personal-post',
+            'excerpt' => 'Lorem ipsum dolor sit amet.',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, voluptatem nesciunt? Nam eligendi dicta nemo exercitationem, repellat molestiae consequuntur magni. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui voluptate eveniet nulla, enim odio perspiciatis omnis placeat temporibus fugit rem.</p>'
+        ]);
     }
 }
