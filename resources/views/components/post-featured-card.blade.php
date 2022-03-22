122f04aa651bc2d7f3fae61eb8f486e8c36efe58
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-03-22 18:31:04 -0400

    Make html posts dynamic through php blade components, attributes, props.

--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -1,41 +1,47 @@
+@props(['post'])
+
 <article class="translation-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
     <div class="py-5 px-4 lg:flex">
     <div class="flex-1 lg:mr-8">
+        {{-- TODO  --}}
         <img src="./images/illustration-1.png" alt="Blog post illustration" class="rounded-xl">
     </div>
 
     <div class="flex flex-1 flex-col justify-between">
         <header class="mt-8 lg:mt-0">
             <div class="space-x-2">
-                <a href="#" class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold" style="font-size: 10px;">Techniques</a>
-                <a href="#" class="px-3 py-1 border border-red-300 rounded-full text-red-300 text-xs uppercase font-semibold" style="font-size: 10px;">Updates</a>
+                <a href="/categories/{{ $post->category->slug }}" class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold" style="font-size: 10px;">{{ $post->category->name }}</a>
             </div>
 
             <div class="mt-4">
-                <h1 class="text-3xl">This is a big title and it will look great on two or even three lines. Woohoo!</h1>
+                <h1 class="text-3xl">
+                    <a href="/posts/{{ $post->slug }}">
+                        {{ $post->title }}
+                    </a>
+                </h1>
                 <span class="mt-2 block text-gray-500 text-xs">
-                    Published <time>1 day ago</time>
+                    Published <time>{{ $post->created_at->diffForHumans() }}</time>
                 </span>
             </div>
         </header>
 
         <div class="text-sm mt-2">
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad fugit quaerat aliquam, molestias assumenda hic sint cum culpa a atque nam quod quis delectus labore dicta reiciendis. Corporis, inventore sunt.</p>
-            
-            <p class="mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, officiis?</p>
+            <p>
+                {{ $post->excerpt }}
+            </p>
         </div>
 
         <footer class="flex justify-between items-center mt-8">
             <div class="flex items-center text-sm">
                 <img src="./images/lary-avatar.svg" alt="Lary avatar">
                 <div class="ml-3">
-                    <h5 class="font-bold">Lary Laracore</h5>
+                    <h5 class="font-bold">{{ $post->author->name }}</h5>
                     <h6>Mascot at Laracast</h6>
                 </div>
             </div>
 
             <div class="hidden lg:block">
-                <a href="post.html" class="text-xs font-semibold hover:bg-gray-400 bg-gray-300 rounded-full  py-2 px-8">Read More</a>
+                <a href="/posts/{{ $post->slug }}" class="text-xs font-semibold hover:bg-gray-400 bg-gray-300 rounded-full  py-2 px-8">Read More</a>
             </div>
         </footer>
 
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,39 +1,15 @@
 <x-layout>
-{{-- 
-    <h1><a href="/">Home Page</a></h1>
-    @foreach ($posts as $post)
-        <article>
-            <h1>
-        <a href="/posts/{{$post->slug}}">
-                    {!!$post->title!!}
-                </a>
-            </h1>
-
-            <p>
-                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-            </p>
-
-            <div>
-                {{ $post->excerpt}}
-            </div>
-        </article>
-    @endforeach --}}
-
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-16 space-y-4">
+        @if ($posts->count())
+        <x-post-grid :posts="$posts"/>
 
-        <x-post-featured-card/>
-
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card/>
-            <x-post-card/>
-        </div>
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card/>
-            <x-post-card/>
-            <x-post-card/>
-        </div>
+        @else
+        <h1 class="text-center">No posts yet. Please check later.</h1>
+        @endif
+        
+        
     </main>
 
 
