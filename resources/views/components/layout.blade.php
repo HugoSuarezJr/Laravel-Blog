76280031cee44e9a563b9014605570635ff5ea98
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-03-25 23:19:51 -0400

    Javascript dropdown menu

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+<script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
 <body style="font-family: Open Sans, sans-serif">
     <section class="px-6 py-8">
