b41228c29a838378c9b3e46d0ca396257599a8ca
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-04-01 10:08:49 -0400

    Tweaks and clean up to post content and categories tab

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,11 +4,11 @@
                     <article class="max-w-4xl mx-auto lg:grid lg:grid-cols-12 gap-x-10">
                         <div class="col-span-4 lg:text-center lg:pt-14 mb-10">
                             <img src="/images/illustration-1.png" alt="" class="rounded-xl">
-        
+
                             <p class="mt-4 block text-gray-400 text-xs">
                                 Published <time>{{ $post->created_at->diffForHumans() }}</time>
                             </p>
-        
+
                             <div class="flex items-center lg:justify-center text-sm mt-4">
                                 <img src="/images/lary-avatar.svg" alt="Lary avatar">
                                 <div class="ml-3 text-left">
@@ -16,7 +16,7 @@
                                 </div>
                             </div>
                         </div>
-        
+
                         <div class="col-span-8">
                             <div class="hidden lg:flex justify-between mb-6">
                                 <a href="/"
@@ -30,21 +30,21 @@
                                             </path>
                                         </g>
                                     </svg>
-        
+
                                     Back to Posts
                                 </a>
-        
+
                                 <div class="space-x-2">
                                     <x-category-button :category="$post->category" />
                                 </div>
                             </div>
-        
+
                             <h1 class="font-bold text-3xl lg:text-4xl mb-10">
                                 {{ $post->title }}
                             </h1>
-        
+
                             <div class="space-y-4 lg:text-lg leading-loose">
-                                {{ $post->body }}
+                                {!! $post->body !!}
                             </div>
                         </div>
                     </article>
