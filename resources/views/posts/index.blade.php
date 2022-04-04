24243e6c7f109501fcfee69bc3f75313bd3de715
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-04-04 13:52:48 -0400

    Extract a Category Dropdown Blade Component

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,12 +1,14 @@
 <x-layout>
-    @include('_posts-header')
+    @include('posts._header')
 
     <main class="max-w-6xl mx-auto mt-16 space-y-4">
         @if ($posts->count())
         <x-post-grid :posts="$posts"/>
 
         @else
-        <h1 class="text-center">No posts yet. Please come back later for some excited news! Please come again!</h1>
+        <h1 class="text-center">
+            No posts yet. Please come back later for some exciting news!
+        </h1>
         @endif
 
 
