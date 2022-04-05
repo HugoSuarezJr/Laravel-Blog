fac7128b71b235e3d958746e3791bde26645e429
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-04-05 12:49:10 -0400

    Login authentication

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,11 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
+
 class SessionsController extends Controller
 {
+    public function create(){
+        return view('sessions.create');
+    }
+
     public function destroy(){
         auth()->logout();
 
         return redirect('/')->with('success', 'Goodbye!');
+    }
+
+    public function store(){
+        // validate the request
+
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        // attempt to authenticate and log in the user
+        if (auth()->attempt($attributes)){
+            // redirect eith a success flash message
+            return redirect('/')->with('success', 'Welcome Back?');
+        }
+        // auth failed
+
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credentials could not be verified'
+        ]);
+
+        // another way to show auth failure
+        // return back()
+        // ->withInput()
+        // ->withErrors(['email' => 'Your provided credentials could not be verified']);
+
     }
 }
