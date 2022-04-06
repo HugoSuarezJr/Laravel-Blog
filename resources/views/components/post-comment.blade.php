ed8fb6c348ee638b24b88621c3272adf4efbaa84
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-04-06 12:32:35 -0400

    Front-end design the comment form

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,21 +1,22 @@
 @props(['comment'])
+<x-panel class="bg-gray-100">
+    <article class="flex space-x-4">
+        <div class="flex-shrink-0">
+            {{-- <img src="https://picsum.photos/100?id={{ $comment->id }}" alt="profile_pic" width="60" height="60" class="rounded-xl"> --}}
+            <img src="https://i.pravatar.cc/150?u={{ $comment->id }}" alt="profile_pic" width="60" height="60" class="rounded-xl">
+        </div>
+        <div>
+            <header class="mb-4">
+                <h3 class="font-bold">{{ $comment->author->username }}</h3>
+                <p class="text-xs">
+                    Posted
+                    <time>{{ $comment->created_at->diffForHumans() }}</time>
+                </p>
+            </header>
 
-<article class="flex bg-gray-100 p-6 border border-gray-200 rounded-xl space-x-4">
-    <div class="flex-shrink-0">
-        <img src="https://picsum.photos/100?id={{ $comment->id }}" alt="profile_pic" width="60" height="60" class="rounded-xl">
-        {{-- <img src="https://i.pravatar.cc/150?u={{ $comment->id }}" alt="profile_pic" width="60" height="60" class="rounded-xl"> --}}
-    </div>
-    <div>
-        <header class="mb-4">
-            <h3 class="font-bold">{{ $comment->author->username }}</h3>
-            <p class="text-xs">
-                Posted
-                <time>{{ $comment->created_at->diffForHumans() }}</time>
+            <p>
+                {{ $comment->body }}
             </p>
-        </header>
-
-        <p>
-            {{ $comment->body }}
-        </p>
-    </div>
-</article>
+        </div>
+    </article>
+</x-panel>
