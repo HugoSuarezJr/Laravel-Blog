f31e02c41bcbd0c31b911f9627c503d0b42af35b
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-04-06 12:03:12 -0400

    Making dynamic comments

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -15,7 +15,7 @@ class CategoryFactory extends Factory
     {
         return [
             'name' => $this->faker->unique()->word,
-            'slug' => $this->faker->slug
+            'slug' => $this->faker->unique()->slug
         ];
     }
 }
--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,18 +1,21 @@
+@props(['comment'])
+
 <article class="flex bg-gray-100 p-6 border border-gray-200 rounded-xl space-x-4">
     <div class="flex-shrink-0">
-        <img src="https://picsum.photos/100" alt="profile_pic" width="60" height="60" class="rounded-xl">
+        <img src="https://picsum.photos/100?id={{ $comment->id }}" alt="profile_pic" width="60" height="60" class="rounded-xl">
+        {{-- <img src="https://i.pravatar.cc/150?u={{ $comment->id }}" alt="profile_pic" width="60" height="60" class="rounded-xl"> --}}
     </div>
     <div>
         <header class="mb-4">
-            <h3 class="font-bold">John Doe</h3>
+            <h3 class="font-bold">{{ $comment->author->username }}</h3>
             <p class="text-xs">
                 Posted
-                <time>8 months ago</time>
+                <time>{{ $comment->created_at->diffForHumans() }}</time>
             </p>
         </header>
 
         <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum laudantium molestias labore repellat tempore ullam maiores earum quas reiciendis et. Voluptatem illo labore quas cum hic ipsum corrupti tempore inventore voluptates incidunt. Odio ullam animi laboriosam facilis quis.
+            {{ $comment->body }}
         </p>
     </div>
 </article>
